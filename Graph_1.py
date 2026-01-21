10ac67d1c9bcfc09018dce2b22ca6b9fff45d176
zayanvali <zayan.vali@icloud.com>
2026-01-21 18:31:11 +0000

    Update Graph_1.py

--- Graph_1.py
+++ Graph_1.py
@@ -12,5 +12,15 @@ class graph:
         queue.append(source)
         visited[source] = True
         while len(queue) > 0:
-            s = queue.pop()
+            s = queue.pop(0)
             res.append(s)
+            for node in self.adj[s]:
+                if visited [node] == False:
+                    queue.append(node)
+                    visited[node] = True
+        return res
+g = graph(4)
+g.create_edge(1, 2)
+g.create_edge(1, 3)
+g.create_edge(2, 4)
+print(g.bsf(0))
